430450e8dcc94ea423a3294c89d435400ed343ad
natalyq1 <onatylili@gmail.com>
2022-11-25 20:55:59 -0500

    Setting routes

--- AppExpress.js
+++ AppExpress.js
@@ -12,3 +12,21 @@ function callback(){
 appe.listen(port,callback)
 //en terminal node AppExpress.js--responde El servidor se esta ejecutando.
 //en navegador http://localhost:3000/ y me sale cannot get pero esta sirviendo
+
+//peticion get con funcion anónima, es como la funcion callback, mas resumida, con 2 parametros
+//request and response
+appe.get("/",(req,res)=> {
+    //respuesta de (funcion anonima)callback
+    res.send("Hola, mundo!")
+}) 
+
+//enviar o redireccionar a /home
+appe.get("/home",(req,res)=> {//enviar o redireccionar a /home
+    //respuesta de (funcion anonima)callback
+    res.send("Estas en la sección Home")
+}) 
+
+appe.get("/home/g1",(req,res)=> {//enviar o redireccionar a /home
+    //respuesta de (funcion anonima)callback
+    res.send("Bienvenido a la clase G1")
+}) 
